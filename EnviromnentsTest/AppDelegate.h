b820ffa0151d72e849e2b3edbed04ba626d80c73
Giovanni Lodi <ing.giovanni.lodi@gmail.com>
2013-03-26 23:51:56 +0000

    First commit

---
+++ EnviromnentsTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EnviromnentsTest
+//
+//  Created by Giovanni Lodi on 3/26/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
